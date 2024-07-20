af0918c22705a601bbadb3f26738cf8f1f88f62e
TatendaTorerwa <ttorerwa01@gmail.com>
2024-07-20 21:21:43 +0200

    feat: Added a set up file.

---
+++ setupenv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# run with source ./setUpenv.sh
+
+export DB_USERNAME='root';
+export DB_PASSWORD='Torerwa1998!';
+export DB_HOST='localhost';
+export DB_PORT='3306';
+export DB_NAME='Jobkonnect';
+export SECRET_KEY='MySecretKey4Jwt';
